35e141f697dc6fff4dc32a61a1ac98a8711b177c
cosascarlos13 <cosascarlos13@gmail.com>
2023-06-19 12:15:39 -0400

    Add project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ComponenteSesion4 
+    v-bind:imagenes="arregloImagenes"
+    v-bind:descripciones="arregloDescripciones"
+    v-bind:autores="arregloAutores"
+    />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import ComponenteSesion4 from './components/ComponenteSesion4.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ComponenteSesion4
+  },
+  data:function(){
+    return{
+      arregloImagenes:[
+        'https://cdn.pixabay.com/photo/2016/02/10/21/59/landscape-1192669_640.jpg',
+        'https://www.dzoom.org.es/wp-content/uploads/2017/07/seebensee-2384369-810x540.jpg',
+        'https://www.10wallpaper.com/wallpaper/medium/2104/Norway_lofoten_mountains_2021_Scenery_5K_Photo_medium.jpg'
+      ],
+      arregloDescripciones:[
+        'PAISAJE 1',
+        'PAISAJE 2',
+        'PAISAJE 3'
+      ],
+      arregloAutores:[
+        'AUTOR 1',
+        'AUTOR 2',
+        'AUTOR 3',
+      ],
+    }
   }
 }
 </script>
---
+++ src/components/ComponenteSesion4.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="componenteSesion4">
+        <h2>Componente Sesión 4</h2>
+        <div id="contenedorPadre">
+            <div id="contenedorIzquierda">
+                <div class="divImagenes" v-for="(imagen, index) in imagenes">
+                    <img class="contenedorImagenes" v-bind:src="imagen" v-bind:alt="index" v-on:click="describirImagen">
+                </div>
+
+            </div>
+            <div v-if="hayClick" id="contenedorDerecha">
+                <div id="contenedorPadreDescripcion">
+                    <div id="descripcionIzquierda">
+                        <img class="contenedorImagenSeleccionada" v-bind:src="imagenSeleccionada"
+                            v-bind:alt="descripcionSeleccionada"><br>
+                        <button v-on:click="cerrarDescripcion">Cerrar</button>
+                    </div>
+                    <div id="descripcionDerecha">
+                        <p>{{ descripcionSeleccionada }}</p>
+                        <p>{{ autorSeleccionado }}</p>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default{
+    name:'ComponenteSesion4',
+    props:{
+        imagenes:{
+            type:Array,
+            required: true,
+        },
+        descripciones:{
+            type: Array,
+        },
+        autores:{
+            type:Array,
+        }
+    },
+    data:function(){
+        return{
+            hayClick:false,
+            imagenSeleccionada:'',
+            descripcionSeleccionada:'',
+            autorSeleccionado:'',
+        }
+    },
+    methods:{
+        describirImagen: function(event){
+            this.hayClick = true;
+            let indiceDeLaImagen= Number(event.target.alt);
+            this.imagenSeleccionada = this.imagenes[indiceDeLaImagen];
+            this.descripcionSeleccionada = this.descripciones[indiceDeLaImagen];
+            this.autorSeleccionado = this.autores[indiceDeLaImagen];
+        },
+        cerrarDescripcion: function(){
+            this.imagenSeleccionada = '';
+            this.descripcionSeleccionada='',
+            this.autorSeleccionado='',
+            this.hayClick = false;
+        },
+
+    }
+}
+</script>
+
+
+<style scoped>
+#contenedorIzquierda{
+    width: 59%;
+    display: inline-block;
+}
+
+#contenedorDerecha{
+    width: 39%;
+    display: inline-block;
+}
+
+.contenedorImagenes{
+    width: 50%;
+}
+
+#descripcionIzquierda{
+    width:69%;
+    display: inline-block;
+}
+
+#descripcionDerecha{
+    width:29%;
+    display: inline-block;
+}
+
+.contenedorImagenSeleccionada{
+    width:60%;
+}
+
+
+</style>
